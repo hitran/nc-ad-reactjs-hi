ab1292e7f29203db67b1edb73613666807d7e538
Hi Tran <thuchitran@gmail.com>
2020-06-25 20:34:10 +0700

    update LAB

--- TypeScript/Day1/hello.js
+++ TypeScript/Day1/hello.js
@@ -2,9 +2,9 @@ var firstName = "Thuc Hi";
 var price = 100;
 var isShown = false;
 var product;
-var productList;
+var fileList;
 product = {
     id: 100,
     name: "Nordic Coder"
 };
-productList = ["hello-world.ts", "hello-world.js"];
+fileList = ["hello-world.ts", "hello-world.js"];
--- TypeScript/Day1/hello.ts
+++ TypeScript/Day1/hello.ts
@@ -2,11 +2,11 @@ const firstName: string = "Thuc Hi";
 const price: number = 100;
 const isShown: boolean = false;
 let product: {id: number, name: string};
-let productList: string[];
+let fileList: string[];
 
 product = {
     id: 100,
     name: "Nordic Coder"
 };
 
-productList = ["hello-world.ts", "hello-world.js"];
+fileList = ["hello-world.ts", "hello-world.js"];
